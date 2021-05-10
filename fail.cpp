fe32c883a23dcb72cfda595395dd149c4c1ec649
Martin Zemla <xzemla1@fi.muni.cz>
2021-05-10 13:06:08 +0200

    fixed some problems2

--- fail.cpp
+++ fail.cpp
@@ -7,23 +7,32 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
   if (sizeof(Static) >= (sizeof(int) * 101)) {
 	Static[100] = 0; 
   }
-	
-  Stack[100] = 0; 
 
-  for (int i = 0; i <= 5; i++) Stack [i] = 0;
+  if (sizeof(Static) >= (sizeof(int) * 101)) {
+	Stack[100] = 0; 
+  }
+
+  for (int i = 0; i < 5; i++) Stack [i] = 0;
 
   int* array = new int[5];
-  array[100] = 0; 
+  int arrayLen = 5;
+  
+  if ((arrayLen * sizeof(int)) >= (sizeof(int) * 101)) {
+	array[100] = 0; 
+  }
 
-  if (sizeof(arrayStack) >= (sizeof(int) * 101)) {
+  if ((arrayStackLen * sizeof(int)) >= (sizeof(int) * 101)) {
 	arrayStack[100] = 0; 
   }
-  arrayHeap[100] = 0; 
+  
+  if ((arrayHeapLen * sizeof(int)) >= (sizeof(int) * 101)) {
+	arrayHeap[100] = 0; 
+  }
 
-  for (unsigned int i = 0; i <= arrayStackLen; i++) {
+  for (unsigned int i = 0; i < arrayStackLen; i++) {
       arrayStack[i] = 0;
   }
-  for (unsigned int i = 0; i <= arrayHeapLen; i++) {
+  for (unsigned int i = 0; i < arrayHeapLen; i++) {
       arrayHeap[i] = 0;
   }
 
